71bba6577afefc0f1235a5e54c8b314eb2bd9a46
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2022-11-22 20:30:26 +0900

    cd.c

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,16 +6,26 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 19:56:59 by gyim              #+#    #+#             */
-/*   Updated: 2022/11/22 20:05:26 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/11/22 20:30:13 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int	main(int argc, char *argv[])
 {
-    if (argc > 2)
-	{c
-		write(2, "Error : too many argument")
+	char	*curr_dir;
+
+	if (argc > 2)
+	{
+		write(2, "Error message\n", 14);
+		exit(1);
 	}
+	
+	curr_dir = getcwd(NULL, 10000);
+	printf("curr_dir : %s\n", curr_dir);
+	free(curr_dir);
+	return (0);
 }
