ff633b53e38142fb937d2b2799c4c0f2d34da538
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2022-11-24 08:03:30 +0900

    1124

--- parser/parser.c
+++ parser/parser.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/22 19:56:59 by gyim              #+#    #+#             */
-/*   Updated: 2022/11/23 08:19:58 by gyim             ###   ########seoul.kr  */
+/*   Created: 2022/11/23 09:42:06 by gyim              #+#    #+#             */
+/*   Updated: 2022/11/24 07:40:58 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../libft/libft.h"
 
-int	main(int argc, char *argv[])
+char	**parser(char *input)
 {
-	char	*result;
-	int		flag;
+	char	**lines;
 
-	if (argc == 1)
-	{
-		flag = chdir("$HOME");
-		result = getcwd(NULL, 1000000);
-		printf("chdir flag : %d\n", flag);
-		printf("changed :dir : %s\n", result);
-		exit(0);
-	}
-	flag = chdir(argv[1]);
-	result = getcwd(NULL, 1000000);
-	printf("chdir flag : %d\n", flag);
-	printf("changed :dir : %s\n", result);
-	return (0);
+	lines = ft_split(input, '\n');
+	return (lines);
 }
