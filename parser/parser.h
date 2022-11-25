6a75ae7c79278a53ce98b453325eaaeb9f38fb18
Gihong Yim <gyim@c1r5s1.42seoul.kr>
2022-11-25 20:34:44 +0900

    gyim

--- parser/parser.h
+++ parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:10:54 by gyim              #+#    #+#             */
-/*   Updated: 2022/11/24 19:32:38 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/11/25 19:27:49 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,50 @@
 # include <unistd.h>
 # include "../libft/libft.h"
 
+typedef enum e_check_list
+{
+	BRACKET,
+	SQUARE_BRACKET,
+	SINGLE_SQUOTE,
+	DOUBLE_SQUOTE,
+	BACKTICK,
+}t_check_list;
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE,
+}t_bool;
+
 typedef struct s_node
 {
-	char	**tokens;
-	char	*op;
-	t_node	*left_node;
-	t_node	*right_node;
+	char			**tokens;
+	// int				op;
+	char			*op;
+	struct s_node	*left;
+	struct s_node	*right;
 }	t_node;
 
-char	**parser(char *input);
-void	change_wspace(char *tmp);
-void	free_line(char **words);
+typedef struct s_stack_node
+{
+	char				data;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}t_stack_node;
 
-//tree.c
-t_node	*make_tree(char **lines);
-int		find_op(char **tokens);
-char	**get_left_tokens(char **tokens, int op_index);
-char	**get_right_tokens(char **tokens, int op_index);
+typedef struct s_stack
+{
+	int				cnt;
+	t_stack_node	*head;
+}t_stack;
 
+// parse_tree.c
+t_node	*parse_tree(char **tokens);
+int		make_child(t_node *parent, char **tokens, int index);
+int		get_tokens_size(char **token);
+int		op_check(char **tokens);
+int		is_op(char *token);
+// parse_tree2.c
+char	**get_left_tokens(char *tokens[], int index);
+char	**get_right_tokens(char *tokens[], int index, int size);
 #endif
