0f748eaacc5156efe650e2d04b05711f1c219678
juha <contemplation.person@gmail.com>
2022-11-27 23:56:27 +0900

    a

--- prompt.c
+++ prompt.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 16:52:21 by juha              #+#    #+#             */
-/*   Updated: 2022/11/27 23:23:32 by juha             ###   ########seoul.kr  */
+/*   Created: 2022/11/27 22:13:33 by juha              #+#    #+#             */
+/*   Updated: 2022/11/27 23:52:49 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/wait.h>
+#include "minishell.h"
 
-#endif
+int	main()
+{
+	char	*sentence;
+
+	while (1)
+	{
+		sentence = readline("a : ");
+		if (sentence == NULL)
+			return (EXIT_SUCCESS); // 에러코드 정리 필요.
+		/* 토큰화 */
+		add_history(sentence);
+		free(sentence);
+		rl_redisplay();
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+	return (EXIT_SUCCESS);
+}
