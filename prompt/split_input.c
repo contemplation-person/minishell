487c3486987a15f09bede389445e403f3072ee17
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2022-11-28 20:33:01 +0900

    gyim 1128

--- prompt/split_input.c
+++ prompt/split_input.c
@@ -6,20 +6,66 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:51:34 by gyim              #+#    #+#             */
-/*   Updated: 2022/11/28 15:48:13 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/11/28 20:32:36 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "prompt.h"
+#include "split_input.h"
 
 char	**split_input(char *input)
 {
-	int	i;
+	int				i;
+	int				input_len;
+	t_tlist_info	*list;
 
+	list = init_tlist();
+	input_len = ft_strlen(input);
 	i = 0;
-	while (input[i])
+	while (i < input_len)
 	{
-		
-		i++;
+		i += add_token(list, input + i);
 	}	
+}
+
+t_tlist_info	*init_tlist(void)
+{
+	t_tlist_info	*list_info;
+
+	list_info = malloc(sizeof(t_tlist_info));
+	if (!list_info)
+		return (NULL);
+	list_info->head = NULL;
+	list_info->cnt = 0;
+	return (list_info);
+}
+
+int	add_token(t_tlist_info *list, char *pos)
+{
+	int	t_start;
+	int	t_end;
+
+	t_start = 0;
+	while (pos[t_start] != '\0' && ft_isspace(pos[t_start]))
+		t_start++;
+	if (pos[t_start] == '\0')
+		return (t_start);
+	t_end = check_token(pos, t_start);
+
+}
+
+void	list_add(t_tlist_info *list, char *str)
+{
+	t_tlist	*new_node;
+
+	new_node = malloc(sizeof(t_tlist));
+	if (!new_node)
+		return ()
+}
+
+int	ft_isspace(char *ch)
+{
+	if (ch == ' ' && ch == '\t')
+		return (1);
+	else
+		return (0);
 }
--- prompt/split_input.h
+++ prompt/split_input.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prompt.h                                           :+:      :+:    :+:   */
+/*   split_input.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:43:32 by gyim              #+#    #+#             */
-/*   Updated: 2022/11/28 11:52:01 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/11/28 20:31:04 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PROMPT_H
-# define PROMPT_H
+#ifndef SPLIT_INPUT_H
+# define SPLIT_INPUT_H
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -19,6 +19,19 @@
 # include <readline/history.h>
 # include "../libft/libft.h"
 
+typedef struct s_tlist
+{
+	char	*token;
+	t_tlist	*next;
+}	t_tlist;
+
+typedef struct s_tlist_info
+{
+	t_list	*head;
+	int		cnt;
+}	t_tlist_info;
+
 #endif
 
 // gcc -lreadline -L../readline -I../readline $(your *.c file)
+
