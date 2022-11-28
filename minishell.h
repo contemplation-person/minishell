781e934f826778caf6af1a2f8965e7ee545f4db4
Juyoung Ha <juha@c2r7s2.42seoul.kr>
2022-11-28 15:58:08 +0900

    next_time continue

--- minishell.h
+++ minishell.h
@@ -6,14 +6,17 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:52:21 by juha              #+#    #+#             */
-/*   Updated: 2022/11/21 17:25:19 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/11/28 15:13:08 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include <readline/readline.h>
+// # include "/Users/juha/goinfre/brew/Cellar/readline/8.2.1/include/readline/readline.h"
+
 # include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
