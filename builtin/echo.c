14141aebca7f3b00881fbd30a0e8b11a75ebcfbc
Juyoung Ha <juha@c2r7s2.42seoul.kr>
2022-11-28 20:47:15 +0900

    done echo

--- builtin/echo.c
+++ builtin/echo.c
@@ -21,26 +21,32 @@ enum e_bool
 int	main(int argc, char **argv)
 {
 	char	*option;
-	int		option_flag;
 	int		option_len;
 	int		i;
 
+	if (argc == 1)
+		ft_putchar_fd('\n', 1);
 	if (argc < 2)
 		return (EXIT_SUCCESS);
 	option_len = 0;
-	option_flag = 0;
-	i = 2;
 	option = argv[1];
 	if ('-' == *option)
 		option_len++;
-	while (option[option_len] != 'n')
+	while (option[option_len] == 'n')
 		option_len++;
-	option_flag = option_len + 1 == ft_strlen(option);
-	if (option_flag == FALSE)
-		ft_putstr_fd(argv[1], STDOUT_FILENO);	
+	if ((option_len == ft_strlen(option)) == FALSE)
+	{
+		ft_putstr_fd(argv[1], STDOUT_FILENO);
+		ft_putchar_fd(' ', STDOUT_FILENO);
+	}
+	i = 2;
 	while (i < argc)
-		ft_putstr_fd(argv[i++], STDOUT_FILENO);	
-	if (option_flag != 1)
+	{
+		ft_putstr_fd(argv[i++], STDOUT_FILENO);
+		if (i < argc)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+	}
+	if ((option_len == ft_strlen(option)) == FALSE)
 		ft_putstr_fd("\n", STDOUT_FILENO);
-	return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
