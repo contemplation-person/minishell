3f24cbdce38520b0a1a5d15f473952108331f55e
Juyoung Ha <juha@c2r7s3.42seoul.kr>
2022-12-07 20:41:01 +0900

    continue unset & export

--- builtin/export.c
+++ builtin/export.c
@@ -6,12 +6,14 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:21:23 by juha              #+#    #+#             */
-/*   Updated: 2022/12/07 11:45:14 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/12/07 20:40:58 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-
+/*
+	같은 값이면, 덮어씀.
+*/
 t_bool	export(t_list **minishell_envp, char **env)
 {
 	ft_lstadd_back(minishell_envp, ft_lstnew(*env));
