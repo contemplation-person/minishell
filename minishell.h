b605fd930c210d33430a012e68fce8c5b697aa42
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2022-12-15 15:49:46 +0900

    ll

--- minishell.h
+++ minishell.h
@@ -6,14 +6,15 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:52:21 by juha              #+#    #+#             */
-/*   Updated: 2022/12/08 19:46:45 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/12/15 15:24:58 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "prompt/prompt.h"
-# include "parser/parser.h"
+# include "parser_list/parser.h"
 # include "libft/libft.h"
+# include "readline.h"
 
 #endif
