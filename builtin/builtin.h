9ad08571b5996e0b36474d874e8c97d7aa21e477
Juyoung Ha <juha@c1r6s3.42seoul.kr>
2022-12-22 16:00:39 +0900

    pwd//done

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:04:03 by juha              #+#    #+#             */
-/*   Updated: 2022/12/21 20:55:50 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/12/22 16:00:19 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int			cd(int argc, char **argv);
 int			echo(int argc, char **argv);
 t_bool		env(t_env_info_list *minishell_envp, char *str); //done
 int			builtin_exit(int argc, char **argv);
-t_bool		builtin_export(t_env_info_list *minishell_envp, char **export_str);
-int			pwd(char *str); //done
-t_bool		builtin_unset(t_env_info_list *l, char **unset_name);
+t_bool		builtin_export(t_env_info_list *minishell_envp, char **export_str); //done
+void		builtin_pwd(char **excute_str_form); //done
+t_bool		builtin_unset(t_env_info_list *l, char **unset_name);//done
 
 #endif
