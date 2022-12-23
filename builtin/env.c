02bd57fef2955c65e9801a8a5ec36ea2fef21a2b
Juyoung Ha <juha@c2r8s2.42seoul.kr>
2022-12-23 16:12:48 +0900

    fix : echo

--- builtin/env.c
+++ builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 12:28:24 by juha              #+#    #+#             */
-/*   Updated: 2022/12/23 15:23:42 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/12/23 16:12:26 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ t_bool	builtin_env(t_env_info_list *minishell_envp, char **excute_str_form)
 
 	i = 0;
 	while (excute_str_form[0])
-	{
 		i++;
-	}
 	if (i > 1)
 	{
 		builtin_error_message("42가", "구현하지말래요", "", 127);
