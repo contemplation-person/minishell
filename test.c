bf91e9c23fcbbea3af125972747b005f053419e1
Juyoung Ha <juha@c2r7s3.42seoul.kr>
2022-12-26 20:47:23 +0900

    mk test

---
+++ test.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+	DIR			*dp;
+	struct dirent *dirp;
+
+	dp = opendir(".");
+	if (dp == NULL)
+	{
+		printf("NULL");
+		return 0;
+	}
+	dirp = readdir(dp);
+	if (dirp == NULL)
+	{
+		printf("NULL");
+		return 0;
+	}
+	while (dirp)
+	{
+		printf("ino : %d\n", dirp->d_ino);
+		printf("d_name : %s\n", dirp->d_name);
+		printf("d_name_len : %d\n", dirp->d_namlen);
+		//printf("%d\n", dirp->d_reclen);
+		//printf("%d\n", dirp->d_seekoff);
+		dirp = readdir(dp);
+	}
+	
+	closedir(dp);
+	char *env = getenv("HOME");
+	printf("this : %s\n", env);
+	write(1,"---------------\n", strlen("---------------\n"));
+
+	if(chdir("~"))
+	{
+		printf("error\n");
+		return 0;
+	}
+	dp = opendir(".");
+	if (dp == NULL)
+	{
+		printf("NULL");
+		return 0;
+	}
+	dirp = readdir(dp);
+	if (dirp == NULL)
+	{
+		printf("NULL");
+		return 0;
+	}
+	while (dirp)
+	{
+		printf("ino : %d\n", dirp->d_ino);
+		printf("d_name : %s\n", dirp->d_name);
+		printf("d_name_len : %d\n", dirp->d_namlen);
+		//printf("%d\n", dirp->d_reclen);
+		//printf("%d\n", dirp->d_seekoff);
+		dirp = readdir(dp);
+	}
+	closedir(dp);
+
+}
