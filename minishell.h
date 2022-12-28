8a500e446c1d86948b092176219bad682b47233a
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2022-12-28 10:20:27 +0900

    logout

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:52:21 by juha              #+#    #+#             */
-/*   Updated: 2022/12/27 15:02:56 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2022/12/28 09:46:17 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/errno.h>
+# include <fcntl.h>
 # include "minishell_struct.h"
 # include "libft/libft.h"
 # include "excute/excute.h"
