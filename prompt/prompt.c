132008ad47bb5775eda9641c137307fb6e17dcb1
Juyoung Ha <juha@c1r4s6.42seoul.kr>
2022-12-31 20:06:08 +0900

    fix : signal 53

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 10:01:26 by juha              #+#    #+#             */
-/*   Updated: 2022/12/31 12:32:06 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/12/31 20:00:34 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,9 @@ static void	init_list(t_env_info_list *list, char **envp)
 	}
 }
 
+//todo : cat처럼 실행이 되는 경우 ^c를 넣으면, 문제가 생김
+//todo : 자식에서 시그널처리를 다시 해줄 필요가 있음.
+//todo : sinal2 에서는 sigquit 시 원래 signal로 돌림, rl_on_new_line 과 rl_replace
 void	signal_handler(int signal_int)
 {
 	if (signal_int == SIGINT)
@@ -37,23 +40,47 @@ void	signal_handler(int signal_int)
 	}
 }
 
-void	_set_signal(struct sigaction *sa)
+void	signal_handler2(int signal_int)
+{
+	if (signal_int == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_redisplay();
+	}
+	else if (signal_int == SIGQUIT)
+	{
+		ft_putendl_fd("Quit: 3", STDERR_FILENO);
+		g_error_code = 131;
+	}
+}
+
+void	_set_signal(struct sigaction *sa, int flag)
 {
 	sa->sa_flags = SIGINFO;
 	sigemptyset(&sa->sa_mask);
 	sigaddset(&sa->sa_mask, SIGQUIT);
 	sigaddset(&sa->sa_mask, SIGINT);
-	sa->__sigaction_u.__sa_handler = signal_handler;
-	signal(SIGQUIT, SIG_IGN);
+	if (flag == 1)
+	{
+		sa->__sigaction_u.__sa_handler = signal_handler;
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, signal_handler);
+	}
+	else
+	{
+		sa->__sigaction_u.__sa_handler = signal_handler2;
+		signal(SIGQUIT, signal_handler2);
+		signal(SIGINT, signal_handler2);
+	}
 }
 
-int	minishell_excute(t_env_info_list *minishell_envp_list)
+int	minishell_excute(t_env_info_list *minishell_envp_list, struct sigaction *sa)
 {
 	char				*sentence;
 
 	while (1)
 	{
-		signal(SIGINT, signal_handler);
+		_set_signal(sa, 1);
 		sentence = readline("MINISHELL : ");
 		if (sentence == NULL)
 		{
@@ -62,13 +89,14 @@ int	minishell_excute(t_env_info_list *minishell_envp_list)
 		}
 		if (sentence && ft_strlen(sentence))
 			add_history(sentence);
+		_set_signal(sa, 0);
 		if (parsing_excute(sentence, minishell_envp_list) == -1)
 		{
 			free(sentence);
 			break ;
 		}
 		free(sentence);
-		// system("leaks minishell");
+		//system("leaks minishell");
 	}
 	return (0);
 }
@@ -81,8 +109,8 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 1)
 		builtin_error_message("bash", "123", "command not found", 127);
 	(void) argv;
-	_set_signal(&sa);
+	//_set_signal(&sa);
 	init_list(&minishell_envp_list, envp);
-	g_error_code = minishell_excute(&minishell_envp_list);
+	g_error_code = minishell_excute(&minishell_envp_list, &sa);
 	return (g_error_code);
 }
