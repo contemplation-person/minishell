250fd6d19e47c8a45311eb6e2492c086e52114e1
Gihong Yim <gyim@c2r7s1.42seoul.kr>
2023-01-02 10:43:19 +0900

    pipe cmds exit code solve

--- operator/pipe_op.c
+++ operator/pipe_op.c
@@ -6,7 +6,7 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 12:42:25 by gyim              #+#    #+#             */
-/*   Updated: 2022/12/29 19:46:53 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2023/01/02 10:11:10 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	op_pipe(t_tree_node *node, t_fds *fd_info, t_env_info_list *env_list)
 {
 	int		fd[2];
 	t_fds	child_fds[2];
+	int		status;
 
 	pipe(fd);
 	child_fds[0].in_fd = fd_info->in_fd;
@@ -26,7 +27,8 @@ int	op_pipe(t_tree_node *node, t_fds *fd_info, t_env_info_list *env_list)
 	op_pipe_right_excute(node, &child_fds[1], fd, env_list);
 	close(fd[0]);
 	close(fd[1]);
-	wait(0);
+	waitpid(0, &status, 0);
+	g_error_code = WEXITSTATUS(status);
 	return (0);
 }
 
@@ -34,6 +36,7 @@ int	op_pipe_left_excute(t_tree_node *node, t_fds *fd_info, int fd[2],
 			t_env_info_list *env_list)
 {
 	int	pid;
+	int	status;
 
 	pid = fork();
 	if (pid == 0)
@@ -41,8 +44,12 @@ int	op_pipe_left_excute(t_tree_node *node, t_fds *fd_info, int fd[2],
 		close(fd[0]);
 		search_tree(node->left, fd_info, env_list);
 		close(fd[1]);
-		exit(0);
+		waitpid(0, &status, 0);
+		g_error_code = WEXITSTATUS(status);
+		exit(g_error_code);
 	}
+	waitpid(0, &status, 0);
+	g_error_code = WEXITSTATUS(status);
 	return (0);
 }
 
@@ -50,6 +57,7 @@ int	op_pipe_right_excute(t_tree_node *node, t_fds *fd_info, int fd[2],
 			t_env_info_list *env_list)
 {
 	int	pid;
+	int	status;
 
 	pid = fork();
 	if (pid == 0)
@@ -57,7 +65,11 @@ int	op_pipe_right_excute(t_tree_node *node, t_fds *fd_info, int fd[2],
 		close(fd[1]);
 		search_tree(node->right, fd_info, env_list);
 		close(fd[0]);
-		exit(0);
+		waitpid(0, &status, 0); 
+		g_error_code = WEXITSTATUS(status);
+		exit(g_error_code);
 	}
+	waitpid(0, &status, 0);
+	g_error_code = WEXITSTATUS(status);
 	return (0);
 }
