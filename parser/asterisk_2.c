293e5070fe5d125b222413b103ac5457beca5252
Juyoung Ha <juha@c2r6s3.42seoul.kr>
2023-01-03 14:26:37 +0900

    asterisk :norm done

--- parser/asterisk_2.c
+++ parser/asterisk_2.c
@@ -6,11 +6,38 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:10:50 by gyim              #+#    #+#             */
-/*   Updated: 2023/01/03 14:07:50 by juha             ###   ########seoul.kr  */
+/*   Updated: 2023/01/03 14:26:02 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+int	analyze_patten(char *sub_token, char *d_name, int *sub_idx, int *d_idx)
+{
+	if (sub_token[(*sub_idx)] == d_name[(*d_idx)])
+	{
+		++(*sub_idx);
+		++(*d_idx);
+	}
+	else if (sub_token[(*sub_idx)] == '*')
+	{
+		if (sub_token[(*sub_idx)])
+			++(*sub_idx);
+	}
+	else if (sub_token[(*sub_idx)] != d_name[(*d_idx)])
+	{
+		if (!(*sub_idx))
+			return (0);
+		else if (sub_token[(*sub_idx) - 1] == '*')
+			(*d_idx)++;
+		else
+			while ((*sub_idx) && sub_token[(*sub_idx) - 1] != '*')
+				--(*sub_idx);
+	}
+	else
+		++(*d_idx);
+	return (1);
+}
 
 int	is_same_patten(char *sub_token, char *d_name)
 {
@@ -21,29 +48,8 @@ int	is_same_patten(char *sub_token, char *d_name)
 	d_idx = 0;
 	while (d_name[d_idx])
 	{
-		if (sub_token[sub_idx] == d_name[d_idx])
-		{
-			++sub_idx;
-			++d_idx;
-			continue ;
-		}
-		else if (sub_token[sub_idx] == '*')
-		{
-			if (sub_token[sub_idx])
-				++sub_idx;
-		}
-		else if (sub_token[sub_idx] != d_name[d_idx])
-		{
-			if (!sub_idx)
-				return (0);
-			else if (sub_token[sub_idx - 1] == '*')
-				d_idx++;
-			else
-				while (sub_idx && sub_token[sub_idx - 1] != '*')
-					--sub_idx;
-		}
-		else
-			++d_idx;
+		if (!analyze_patten(sub_token, d_name, &sub_idx, &d_idx))
+			return (0);
 	}
 	while (sub_token[sub_idx] == '*')
 		sub_idx++;
